1456d17ad9648b0e29b9d0a5029386f5284be9e9
David lashlee <davidlashlee@gmail.com>
2022-12-03 21:45:23 -0700

    finished day3 part 2

---
+++ days/day3/part1.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+
+let satchleData = fs.readFileSync("data.txt", "utf-8").split("\n");
+
+
+const divideSatchle = (satchle: string): string[] => {
+  let satchleLength = satchle.length;
+  let firstHalf = satchle.substring(0, satchle.length / 2);
+  let secondHalf = satchle.substring(satchleLength / 2);
+  return [firstHalf, secondHalf];
+};
+
+const findMatchingSymbol = (twoSatchles: string[]): string | null => {
+  if (twoSatchles[1] && twoSatchles[0]) {
+    for (let i = 0; i < twoSatchles[0].length; i++) {
+      if (typeof twoSatchles[0][i] === "string") {
+        let letter = twoSatchles[0][i];
+        if (letter) {
+          if (twoSatchles[1].includes(letter)) {
+            return letter;
+          }
+        }
+      }
+    }
+  }
+  return null;
+};
+
+const getScore = (letter: string | null): number | null => {
+  if (letter) {
+    const ascii = letter.charCodeAt(0);
+    if (letter.toUpperCase() === letter) {
+      let score = ascii - 38;
+      return score;
+    } else {
+      let score = ascii - 96;
+      return score;
+    }
+  }
+  return null;
+};
+let total = 0;
+satchleData.forEach((satchle) => {
+  let score = getScore(findMatchingSymbol(divideSatchle(satchle)));
+  if (score) {
+    total += score;
+  }
+});
+
+console.log(total);
--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
+var lodash_1 = require("lodash");
+var errors = 0;
 var satchleData = fs.readFileSync("data.txt", "utf-8").split("\n");
-console.log(satchleData);
-var divideSatchle = function (satchle) {
-    var satchleLength = satchle.length;
-    var firstHalf = satchle.substring(0, satchle.length / 2);
-    var secondHalf = satchle.substring(satchleLength / 2);
-    return [firstHalf, secondHalf];
+var chopArray = function (data, subsetSize) {
+    if (subsetSize === void 0) { subsetSize = 3; }
+    var chopped = (0, lodash_1.chunk)(data, subsetSize);
+    chopped.pop();
+    return chopped;
 };
-var findMatchingSymbol = function (twoSatchles) {
-    if (twoSatchles[1] && twoSatchles[0]) {
-        for (var i = 0; i < twoSatchles[0].length; i++) {
-            if (typeof twoSatchles[0][i] === "string") {
-                var letter = twoSatchles[0][i];
-                console.log("letter", letter, "satchles", twoSatchles, "i", i);
-                if (letter) {
-                    if (twoSatchles[1].includes(letter)) {
-                        return letter;
-                    }
+var findMatchingSymbol = function (satchles) {
+    console.log("findingSymbols in ", satchles);
+    if (satchles && satchles[0] && satchles[1] && satchles[2]) {
+        for (var i = 0; i < satchles[0].length; i++) {
+            if (typeof satchles[0][i] === "string") {
+                var letter = satchles[0][i];
+                if (satchles[1].includes(letter) && satchles[2].includes(letter)) {
+                    console.log("found a match", letter);
+                    return letter;
                 }
             }
         }
     }
-    return null;
+    console.log("ERROR in findMatchingSymbolv2");
+    errors++;
+    return "errors";
 };
 var getScore = function (letter) {
     if (letter) {
         var ascii = letter.charCodeAt(0);
         if (letter.toUpperCase() === letter) {
-            // i dont know the right number to subtract from the ascii to make score argh!!1
             var score = ascii - 38;
-            console.log("asci uppercase", letter, "score", score);
             return score;
         }
         else {
             var score = ascii - 96;
-            console.log("asci lowercase", letter, "score", score);
             return score;
         }
     }
     return null;
 };
 var total = 0;
-satchleData.forEach(function (satchle) {
-    var score = getScore(findMatchingSymbol(divideSatchle(satchle)));
+chopArray(satchleData).forEach(function (satchelSet) {
+    var score = getScore(findMatchingSymbol(satchelSet));
     if (score) {
         total += score;
     }
 });
 console.log(total);
+console.log(errors);
